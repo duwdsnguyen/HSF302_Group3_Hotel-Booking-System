4b0822182f67d05d37dec16e6c025e42566cfdcd
nguyenlebinhan <nguyenlebinhank63@gmail.com>
2026-07-03 12:13:38 +0700

    Add git ignore

--- src/main/java/hsf/g3/hotel_booking_system/security/WebSecurityConfig.java
+++ src/main/java/hsf/g3/hotel_booking_system/security/WebSecurityConfig.java
@@ -2,9 +2,11 @@ package hsf.g3.hotel_booking_system.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -13,4 +15,13 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(auth ->
+                auth.requestMatchers("/login","/register","/css/**","/js/**").permitAll().anyRequest().authenticated())
+                .formLogin(form -> form.loginPage("/v1/auth/login").permitAll());
+        return http.build();
+    }
+
 }
